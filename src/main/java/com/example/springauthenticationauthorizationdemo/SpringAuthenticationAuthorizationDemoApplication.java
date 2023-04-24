7ad48c592e4455093b08f22bc8454e25b53b4d61
kradhakr <kradhakr@users.noreply.github.com>
2023-04-24 14:36:42 +0200

    Add files via upload

---
+++ src/main/java/com/example/springauthenticationauthorizationdemo/SpringAuthenticationAuthorizationDemoApplication.java
@@ -0,0 +1,15 @@
+package com.example.springauthenticationauthorizationdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@SpringBootApplication
+@EnableWebSecurity
+public class SpringAuthenticationAuthorizationDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAuthenticationAuthorizationDemoApplication.class, args);
+	}
+
+}
